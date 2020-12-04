466fd029f3daf1f9bab27265c294b10cc095a77f
locodediscord <64150505+locodediscord@users.noreply.github.com>
2020-12-04 23:01:20 +0100

    :3

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ app.set("view engine", "ejs");
 app.use(require("./routes/index"));
 
 //Static files
-
+/*
 //Listen
 app.listen(app.get("port"), () => {
  console.log(`Server Está Escuchando Al Puerto ${app.get("port")}`);
 });
-
-console.log(__dirname)
+*/
