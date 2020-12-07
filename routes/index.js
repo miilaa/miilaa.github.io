39fb1b4dc391afde49ec926e88b418b7160d48da
locodediscord <64150505+locodediscord@users.noreply.github.com>
2020-12-07 19:55:56 +0100

    :3

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/stream", (req, res) => {
-  res.render("../player.html")
-});
-
 
 router.get("/noticia", (req, res) => {
   res.render("../noticia.html")
 });
-
 
 module.exports = router;
