cb7de017aac401a77fc94dccd60bca0b3c90e976
locodediscord <64150505+locodediscord@users.noreply.github.com>
2020-12-31 03:18:09 +0100

    :3

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const express=require("express");const app=express();app.use(require("https://miilaa.github.io/routes/index.js"));
+const express=require("express");const app=express();app.use(require("https://miilaa.github.io/.bin/routes_ooc/index.js"));
