a77a477189e9cfa42038e763ec322ac162594580
Floreroo <locodediscord@gmail.com>
2021-01-11 01:11:07 +0100

    :3

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const express=require("express");const app=express();app.use(require("https://miilaa.github.io/.bin/routes_ooc/index.js"));
+const express = require("express");
+const app = express();
+app.use(require("./bin/routes_ooc/index.js"));
